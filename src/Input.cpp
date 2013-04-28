f9537f28f810c45d44b39677f8862877f0fff846
Yuval Gitlitz <gitlitzo@gmail.com>
2013-04-28 14:21:40 +0300

    input support keypressed, keydown

--- src/Input.cpp
+++ src/Input.cpp
@@ -7,17 +7,28 @@
 
 #include "Input.h"
 #include "stdio.h"
+#include "debug settings.h"
 Input* Input::instance=NULL;
 Input* Input::GetInstance() {
-	puts("i was here");
 	if(instance==NULL)
 		instance=new Input();
 	return instance;
 }
 
-Input::Input() {
-	// TODO Auto-generated constructor stub
+bool Input::IsKeyDown(EKEY_CODE keyCode) const {
+	return KeyIsDown[keyCode];
+}
 
+bool Input::IsKeyPressed(EKEY_CODE keyCode) const {
+	return KeyIsDown[keyCode];
+}
+
+Input::Input() {
+	for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+	{
+		KeyIsDown[i] = false;
+		KeyIsPressed[i] = false;
+	}
 }
 
 Input::~Input() {
@@ -25,5 +36,19 @@ Input::~Input() {
 }
 
 bool Input::OnEvent(const SEvent& event) {
+	if (event.EventType == EET_KEY_INPUT_EVENT)
+	{
+		if(KeyIsDown[event.KeyInput.Key] != event.KeyInput.PressedDown)
+		{
+			KeyIsPressed[event.KeyInput.Key]= event.KeyInput.PressedDown;
+#ifdef DEBUG_KEY_PRESSED
+			if(event.KeyInput.PressedDown)
+				printf("key pressed %d \n",event.KeyInput.Key);
+#endif
+		}
+		else
+			KeyIsPressed[event.KeyInput.Key]=false;
+		KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+	}
 	return false;
 }
---
+++ src/debug settings.h
@@ -0,0 +1,13 @@
+/*
+ * debug settings.h
+ *
+ *  Created on: 28 באפר 2013
+ *      Author: yuval
+ */
+
+#ifndef DEBUG_SETTINGS_H_
+#define DEBUG_SETTINGS_H_
+
+#define DEBUG_KEY_PRESSED
+
+#endif /* DEBUG_SETTINGS_H_ */
